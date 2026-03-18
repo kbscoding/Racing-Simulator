fdd1383da24b4c9b7917755a48d17f4277a5cd65
kbscoding <kofikbempong@gmail.com>
2026-03-18 22:11:19 +0000

    Create BaseScreen class with basic methods

---
+++ screens/base_screen.py
@@ -0,0 +1,15 @@
+class BaseScreen:
+    def __init__(self, game):
+        self.game = game
+
+    def handle_event(self, event):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
+    def on_resize(self):
+        pass
